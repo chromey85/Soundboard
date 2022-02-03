24f0b7b70ab1e51c7104054f08ed737c4f1f9651
chromey85 <yayyub85@gmail.com>
2022-02-03 23:30:19 +0000

    update

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-const sounds = document.querySelector("#sega_logo")
+const all = document.querySelector(".All");
+const text = document.querySelector(".main_title");
+// sms
+// sms1
+// sms2
+// smd
+// smd1
+// smd2
+// nin
+// nes1
+// nes2
+// snes
+// snes1
+// snes2
+// sps
+// sps1
+// sps20
+// sps21
+// sps22
+// msx
+// msx1
+// msx2
+// msx360
+// msx3601
+// msx3602
 
-sounds.addEventListener("click", s => {
-    console.log('#sega_logo')
+document.addEventListener("click", (i) => {
+    console.log(i);
+    all.style.display = "flex";
+    text.style.display = "none";
 })
